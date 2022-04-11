049e5579d02ea1e5df1e1139318b9016f9bcba25
PavithraKaruppiah <101961073+PavithraKaruppiah@users.noreply.github.com>
2022-04-12 02:47:11 +0300

    Add files via upload

---
+++ MultipleLanguage.java
@@ -0,0 +1,15 @@
+package week3.day2;
+
+import org.apache.poi.util.SystemOutLogger;
+
+public abstract class MultipleLanguage   {
+//unimplemented Method
+	abstract void ruby();
+	
+	//implemented Method
+	
+	public void phython() {
+		System.out.println("Phython");
+	}
+	
+}
